7105fc796474cc0a062e09fc1db7a60a9b475832
shriniwas tiwari <tiwariritik747@gmail.com>
2022-07-08 18:04:18 +0530

    phadi_food
    this is food ordering  ecommerce website

---
+++ constant.inc.php
@@ -0,0 +1,13 @@
+<?php
+define('SITE_NAME','Food Ordering Admin');
+define('FRONT_SITE_NAME','Food Ordering ');
+//define('FRONT_SITE_NAME','Food Ordering' );
+define('FRONT_SITE_PATH','http://localhost/phadi_food/');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/phadi_food/");
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+
+define('SITE_IMAGE',"http://localhost/phadi_food/");
+define('SITE_DISH_IMAGE',FRONT_SITE_PATH."media/dish/");
+define('SERVER_BANNER_IMAGE',SERVER_IMAGE."media/banner/");
+define('SITE_BANNER_IMAGE',FRONT_SITE_PATH."media/banner/");
+?>
